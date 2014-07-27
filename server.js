354c12a98b0f32063853d09aba5ca6c48e1ea8d6
Bryan Yap <bryan.yap@alliants.com>
2014-07-27 09:52:27 +0100

    updated to have a sensible routing system

--- server.js
+++ server.js
@@ -4,7 +4,21 @@ var fs = require('fs'),
     express = require('express'),
     app = express();
 
+app.engine('.html', require('jade').renderFile);
 app.set('title', 'Salesforce Summer of Hacks (Local Test Environment)');
-app.use('/', express.static(__dirname));
+app.use('/assets/', express.static(__dirname + '/assets/'));
+app.use('/lib/', express.static(__dirname + '/lib/'));
+
+app.get('/', function(req, res) {
+  res.sendfile('views/create-group.html');
+});
+
+app.get('/groups/find', function(req, res) {
+  res.sendfile('views/find-group.html');
+});
+
+app.get('/groups/create', function(req, res) {
+  res.sendfile('views/create-group.html');
+});
 
 app.listen(3000);
